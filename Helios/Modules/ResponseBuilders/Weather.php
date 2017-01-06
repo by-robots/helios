a03a95ed7df4492f1893575d9f2dd7d45aadc68f
By Robots <skynet@by-robots.com>
2017-01-06 22:08:26 +0000

    Start friendly responses.

---
+++ Helios/Modules/ResponseBuilders/Weather.php
@@ -0,0 +1,16 @@
+<?php namespace Helios\Modules\ResponseBuilders;
+
+class Weather implements Response
+{
+    /**
+     * Build a friendly response about the current weather.
+     *
+     * @param mixed $input
+     *
+     * @return string
+     */
+    public function respond($input)
+    {
+        //
+    }
+}
